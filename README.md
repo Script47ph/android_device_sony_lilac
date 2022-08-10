5a86341f5fabf43bfa414c81167942947d0bfd58
Script47 <54595497+Script47ph@users.noreply.github.com>
2022-08-10 09:02:31 +0700

    Update for Project Elixir ROM

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ How to build
         <!-- SONY -->
         <project name="whatawurst/android_kernel_sony_msm8998" path="kernel/sony/msm8998" remote="github" revision="lineage-19.1" />
         <project name="Havoc-Devices/android_device_sony_yoshino-common" path="device/sony/yoshino-common" remote="github" revision="twelve" />
-        <project name="Script47ph/android_device_sony_lilac" path="device/sony/lilac" remote="github" revision="aosp-12" />
+        <project name="Script47ph/android_device_sony_lilac" path="device/sony/lilac" remote="github" revision="ProjectElixir-12.1" />
 
         <!-- blobs for lilac -->
         <project name="Havoc-Devices/android_vendor_sony_lilac" path="vendor/sony/lilac" remote="github" revision="twelve" />
--- aosp_lilac.mk
+++ aosp_lilac.mk
@@ -8,23 +8,35 @@ $(call inherit-product, device/sony/lilac/device.mk)
 # Product API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
-### BOOTANIMATION
-# vendor/aosp/config/common_full_phone.mk
+# Display Resolution
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
+
+# Boot Resolution
 TARGET_BOOT_ANIMATION_RES := 720
-# vendor/aosp/config/common.mk
 TARGET_BOOTANIMATION_HALF_RES := true
 
-### Aosp Stuffs
+# Quick tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# FaceUnlockService
+TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK := false
+
+# ProjectElixir stuffs
+ELIXIR_BUILD_TYPE := UNOFFICIAL
+ELIXIR_MAINTAINER := Script47ph
+
+# Aosp Stuffs
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_PIXEL_WALLPAPER := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 IS_PHONE := true
-
-### FaceUnlockService
-TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK := false
 
 ### AOSP
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
@@ -41,7 +53,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lilac \
     PRIVATE_BUILD_DESC="G8441-user 9 47.2.A.11.228 3311891731 release-keys" \
     BUILD_NUMBER=8650216
-
 
 BUILD_FINGERPRINT := Sony/G8441/G8441:9/47.2.A.11.228/3311891731:user/release-keys
 
